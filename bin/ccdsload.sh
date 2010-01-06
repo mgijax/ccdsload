361d232910063db1b7bd19834264b91e0e4f84c7
Lori Corbani <Lori.Corbani@jax.org>
2010-01-06 18:45:07 +0000

    TR9932

---
+++ bin/ccdsload.sh
@@ -0,0 +1,118 @@
+#!/bin/sh
+#
+#  ccdsload.sh
+###########################################################################
+#
+#  Purpose:
+# 	This script creates CCDS assocload
+#       input file and invokes assocload
+#
+  Usage=ccdsload.sh
+#
+#  Env Vars:
+#
+#      See the configuration file
+#
+#  Inputs:
+#
+#      - Common configuration file -
+#               /usr/local/mgi/live/mgiconfig/master.config.sh
+#      - CCDS load configuration file - ccdsload.config
+#      - input file - see python script header
+#
+#  Outputs:
+#
+#      - An archive file
+#      - Log files defined by the environment variables ${LOG_PROC},
+#        ${LOG_DIAG}, ${LOG_CUR} and ${LOG_VAL}
+#      - Input files for vocload and assocload
+#      - see vocload and assocload outputs
+#      - Records written to the database tables
+#      - Exceptions written to standard error
+#      - Configuration and initialization errors are written to a log file
+#        for the shell script
+#
+#  Exit Codes:
+#
+#      0:  Successful completion
+#      1:  Fatal error occurred
+#      2:  Non-fatal error occurred
+#
+#  Assumes:  Nothing
+#
+# History:
+#
+# lec	01/06/2010 - created
+#
+
+cd `dirname $0`
+LOG=`pwd`/ccdsload.log
+rm -rf ${LOG}
+
+CONFIG_LOAD=../ccdsload.config
+
+#
+# verify & source the configuration file
+#
+
+if [ ! -r ${CONFIG_LOAD} ]
+then
+    echo "Cannot read configuration file: ${CONFIG_LOAD}"
+    exit 1
+fi
+
+. ${CONFIG_LOAD}
+
+#
+#  Source the DLA library functions.
+#
+
+if [ "${DLAJOBSTREAMFUNC}" != "" ]
+then
+    if [ -r ${DLAJOBSTREAMFUNC} ]
+    then
+        . ${DLAJOBSTREAMFUNC}
+    else
+        echo "Cannot source DLA functions script: ${DLAJOBSTREAMFUNC}" | tee -a ${LOG}
+        exit 1
+    fi
+else
+    echo "Environment variable DLAJOBSTREAMFUNC has not been defined." | tee -a ${LOG}
+    exit 1
+fi
+
+#####################################
+#
+# Main
+#
+#####################################
+
+#
+# createArchive including OUTPUTDIR, startLog, getConfigEnv
+# sets "JOBKEY"
+preload ${OUTPUTDIR}
+
+#
+# create input files
+#
+#echo 'Running createInputFiles.py' >> ${LOG_DIAG}
+#${CCDSLOAD}/bin/createInputFiles.py >> ${LOG_DIAG}
+#STAT=$?
+#checkStatus ${STAT} "${CCDSLOAD}/bin/createInputFiles.py"
+
+#
+# run association load
+#
+
+# set to full path for assocload
+CONFIG_LOAD=${CCDSLOAD}/ccdsload.config
+
+echo "Running CCDS association load" >> ${LOG_DIAG}
+${ASSOCLOADER_SH} ${CONFIG_LOAD} ${JOBKEY}
+STAT=$?
+checkStatus ${STAT} "${ASSOCLOADER_SH}/bin/AssocLoad.sh ${CONFIG_LOAD}"
+
+#
+# run postload cleanup and email logs
+#
+shutDown
---
+++ bin/createInputFiles.py
@@ -0,0 +1,125 @@
+#!/usr/local/bin/python
+
+##########################################################################
+#
+# Purpose:
+#       From CCDSO input file create assocload input file
+#
+# Usage: createInputFiles.py
+# Env Vars:
+#	 1. OUTFILE_NAME
+#
+# Inputs:
+#	CCDS.current.txt
+#
+#	   field 3: gene
+#	   field 4: gene id
+#
+# Outputs:
+#	 A tab-delimited file in assocload format:
+#
+#	    line 1 header: "MGI" "CCDS"
+#
+# 	    field 1: MGI ID
+#           field 2: CCDS ID
+# 
+# Exit Codes:
+#
+#      0:  Successful completion
+#      1:  An exception occurred
+#
+#  Assumes:  Nothing
+#
+#  Notes:  None
+#
+###########################################################################
+
+import sys
+import os
+import mgi_utils
+import string
+import db
+
+print '%s' % mgi_utils.date()
+
+# paths to input and two output files
+inFilePath = os.environ['INFILE_NAME_CCDS']
+assocFilePath= os.environ['INFILE_NAME']
+
+# file descriptors
+
+# input
+inFile = ''
+# output for assocload
+assocFile = ''
+
+TAB= '\t'
+CRT = '\n'
+SPACE = ' '
+
+#
+# Initialize
+#
+
+try:
+    inFile = open(inFilePath, 'r')
+except:
+    exit('Could not open file for reading %s\n' % inFilePath)
+
+try:
+    assocFile = open(assocFilePath, 'w')
+except:
+    exit('Could not open file for writing %s\n' % assocFilePath)
+
+#
+# Get Primary Mouse Markers/EntrezGene IDs
+#
+results = db.sql('''
+        select a.accID, a._Object_key, mgiID = aa.accID
+        from ACC_Accession a, ACC_Accession aa, MRK_Marker m
+        where a._MGIType_key = 2 
+        and a._LogicalDB_key = 55 
+        and a.preferred = 1
+        and a._Object_key = aa._Object_key
+        and aa._MGIType_key = 2 
+        and aa._LogicalDB_key = 1
+        and aa.preferred = 1
+	and a._Object_key = m._Marker_key
+	and m._Organism_key = 1
+	and m._Marker_Status_key != 2
+        ''', 'auto')
+egID = {}
+for r in results:
+    key = r['accID']
+    value = r['mgiID']
+    egID[key] = value
+
+#
+# Process
+#
+
+# write out assocload header
+assocFile.write('%s%s%s%s' % ('MGI', TAB, 'CCDS', CRT))
+
+# throw away header line
+header = inFile.readline()
+
+for line in inFile.readlines():
+
+    tokens = string.split(line, TAB)
+    gene = tokens[2]
+    ccdsID = string.replace(tokens[3], 'CCDS', '')
+
+    if egID.has_key(ccdsID):
+        assocFile.write('%s%s%s%s' % (egID[ccdsID], TAB, ccdsID, CRT))
+    else:
+	print 'Invalid EntrezGene ID:  %s, %s\n' % (gene, ccdsID)
+
+#
+# Post Process
+#
+
+inFile.close()
+assocFile.close()
+
+print '%s' % mgi_utils.date()
